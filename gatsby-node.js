8937efa90ee01b778b4d63c4cc0af98bf0c42986
Test <test@MacBook-Pro-von-Test.local>
2025-05-04 18:42:46 +0200

    Remove all files from repository

--- gatsby-node.js
+++
@@ -1,9 +0,0 @@
-exports.createSchemaCustomization = ({ actions }) => {
-  const { createTypes } = actions;
-  createTypes(`
-    type SiteSiteMetadata {
-      title: String
-      author: String
-    }
-  `);
-};
