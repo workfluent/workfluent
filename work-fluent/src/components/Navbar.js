494baa7fbe06017d894995663cee9514971dd086
Test <test@MacBook-Pro-von-Test.local>
2025-05-06 08:51:11 +0200

    navbar done

--- work-fluent/src/components/Navbar.js
+++ work-fluent/src/components/Navbar.js
@@ -10,10 +10,10 @@ const Navbar = () => {
       {/* Navigation Links */}
       <div className={styles.navLinksContainer}>
         <div className={styles.navLinks}>
-          <a href="#services" className={styles.navLink}>Services</a>
-          <a href="#process" className={styles.navLink}>Process</a>
-          <a href="#contact" className={styles.navLink}>Contact</a>
-          <a href="#about" className={styles.navLink}>About Us</a>
+          <a href="#services" className={`${styles.navLink} ${styles.navButton}`}>Services</a>
+          <a href="#process" className={`${styles.navLink} ${styles.navButton}`}>Process</a>
+          <a href="#contact" className={`${styles.navLink} ${styles.navButton}`}>Contact</a>
+          <a href="#about" className={`${styles.navLink} ${styles.navButton}`}>About Us</a>
         </div>
       </div>
     </div>
