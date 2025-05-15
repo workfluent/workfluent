c6a70b18d5d637fdc5f682b51438bb494ba64121
Test <test@MacBook-Pro-von-Test.local>
2025-05-15 22:26:51 +0200

    deploy github

--- work-fluent/gatsby-config.js
+++ work-fluent/gatsby-config.js
@@ -4,9 +4,11 @@ module.exports = {
     description: `Your site description`,
     author: `Your Name`,
   },
+  pathPrefix: "/workfluent", // Ensure this matches your GitHub repository name
   plugins: [
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-postcss`, // Ensure PostCSS plugin is included for Tailwind CSS
     {
       resolve: `gatsby-plugin-styled-components`,
       options: {
